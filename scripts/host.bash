170c8c261d0df4543645370f4b1730191f861081
Todd Yamakawa <todd.yamakawa@arm.com>
2019-11-08 09:15:22 -0600

    host: Changing color for weird host.

--- scripts/host.bash
+++ scripts/host.bash
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-# --- LSF Environment ---
+# Variables
 job=$LSB_BATCH_JID
+host=$(hostname --short)
+
+# --- LSF Environment ---
 if [[ -n $job ]]; then
 	# TODO: time_left=$(bjobs -noheader -o time_left $job)
 	echo "#[fg=colour11][$job]#[fg=default]"
 
-# --- Hostname ---
+# --- Weird Host ---
+elif [[ $host =~ ^s110 ]]; then
+	echo "#[fg=colour9][$(hostname --short)]#[fg=default]"
+
+# --- Regular Host ---
 else
 	echo "#[fg=colour10][$(hostname --short)]#[fg=default]"
 fi
