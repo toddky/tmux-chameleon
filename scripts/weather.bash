4b1102aa91410bd892d03f8baf05b5fd77c16d5c
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-05-31 17:47:50 -0500

    scripts/weather.bash: Adding icons.

--- scripts/weather.bash
+++ scripts/weather.bash
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 # https://github.com/chubin/wttr.in
+
+declare -A weather_icons=(
+    ["Clear"]="󰖙"
+    ["Cloud"]=""
+    ["Drizzle"]="󰖗"
+    ["Fog"]=""
+    ["Haze"]="󰼰"
+    ["Mist"]=""
+    ["Overcast"]=""
+    ["Rain"]=""
+    ["Sand"]=""
+    ["Shower"]=""
+    ["Smoke"]=""
+    ["Snow"]=""
+    ["Sunny"]="󰖙"
+    ["Thunderstorm"]=""
+    ["Tornado"]="󰼸"
+    ["Windy"]="󰖝"
+)
+
 format="%l+-+%C+%t+%w"
 weather="$(curl "http://wttr.in/Austin?u&format=$format" 2>/dev/null)"
 RETVAL=$?
